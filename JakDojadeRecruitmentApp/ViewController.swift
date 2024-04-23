186cf04f2a3a6f370587e6c33a191524a5a9b8d1
Jacek Stąporek <roller.jacek@gmail.com>
2024-04-23 08:50:57 +0200

    Initial project commit

---
+++ JakDojadeRecruitmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JakDojadeRecruitmentApp
+//
+//  Created by Jacek Stąporek on 23/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
